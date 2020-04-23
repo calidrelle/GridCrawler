b11d117e3fd0e13e28f0c4121fa1d831e256734d
calidrelle <stephane.stephanos@gmail.com>
2020-04-23 15:11:25 +0200

    Suppression de fichier graphiques obsolètes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GridCrawler
+
+Participation à la Game Jam #24 de GameCodeur
+https://itch.io/jam/gamecodeur-gamejam-24
---
+++ gameobjects/itemFactory.lua
@@ -0,0 +1,8 @@
+local factory
+
+factory.create = function()
+    local item = {}
+    return item
+end
+
+return factory
