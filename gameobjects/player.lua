6953858239331ddf7986b7c41058dc5cd3cd830a
calidrelle <stephane.stephanos@gmail.com>
2020-05-15 00:43:12 +0200

    changelog et Player commence au niveau 1

--- gameobjects/player.lua
+++ gameobjects/player.lua
@@ -14,11 +14,7 @@ FRICTION = 0.55
 
 this.createNew = function()
     this.name = "player"
-    if DevMode() then
-        this.level = 10
-    else
-        this.level = 1
-    end
+    this.level = 1
     this.x = 0
     this.y = 0
     this.bounds.x = 7
